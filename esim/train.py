3f2ea652fe596feb74bb50845f229b7394693712
BAIRONG ZHUANG <marvinzh6019@gmail.com>
2019-07-29 18:56:22 +0900

    fix bugs

--- esim/train.py
+++ esim/train.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     train_data = DataLoader(train_raw, batch_size=args.batch_size, shuffle=True, collate_fn=padding_for_SNLI)
     valid_data = DataLoader(valid_raw)
 
-    model = ESIM(train_raw.size, args.d_embed, args.d_hidden, args.n_layers,
+    model = ESIM(train_raw.lang.size, args.d_embed, args.d_hidden, args.n_layers,
                  args.d_proj, args.d_v, args.n_layers_cmp, args.d_pred, args.dropout_rate)
     loss_fn = torch.nn.CrossEntropyLoss()
     optim = torch.optim.Adam(model.parameters())
